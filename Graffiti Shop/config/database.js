6b2b6a9a81196bc01bbe4eb24cd4a40d5363bb47
WholesomeAhki <Victor.D.Jackson@gmail.com>
2022-11-07 01:23:57 -0500

    first commit needs a simple fix to the message up function and a fetch for put

---
+++ Graffiti Shop/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://VictorAzizJ:Bahiyah1@cluster0.ed64pxn.mongodb.net/Bag-and-Tag?retryWrites=true&w=majority', 
+    'dbName': 'Bag-and-Tag'
+};
---
+++ Graffiti Shop/public/main.js
@@ -0,0 +1,105 @@
+//var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash-o");
+//make listener for thumbs down//
+//var thumbDown = document.getElementsByClassName("fa-thumbs-down")
+let updateStaff = document.getElementsByClassName('customerService')
+/*Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp': true
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+//make thumbs down function//
+Array.from(thumbDown).forEach(function(element){
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    
+    fetch('messages', {
+      method: 'put',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({
+        'name' : name,
+        'msg' : msg,
+        'thumbUp' : false
+      })
+      
+
+
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+
+  })
+
+
+
+
+
+})*/
+Array.from(updateStaff).forEach(function(element) {
+  element.addEventListener('click', function(){
+  
+    const update = this.parentNode.parentNode.childNodes[1].childNodes[9].value
+  
+    const msg = this.parentNode.getAttribute('data-id')
+    
+    fetch('messages', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'Message': update,
+        'msg': msg,
+        
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+      
+        const msg = this.parentNode.parentNode.getAttribute('data-id')
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+          //maybe comment out so available to look at object
+        })
+      });
+});
